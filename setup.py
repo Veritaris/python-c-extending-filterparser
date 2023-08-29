3f0c0973ca52abad5547174f6eb7a619feaae89b
georgii.imeshkenov <georgiiimeshkenov@gmail.com>
2023-08-29 10:47:02 +0300

    some small codestyle

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from setuptools import setup, Extension
 
 module = Extension(
     "filterparser",
-    sources=["library.c", "struct.c", ]
+    sources=[
+        "library.c",
+        "struct.c",
+    ]
 )
 
 setup(
     name="FilterParser",
     version="0.1.0",
+    author="Georgii Imeshkenov",
     description="nd",
-    ext_modules=[module]
+    ext_modules=[module],
 )
